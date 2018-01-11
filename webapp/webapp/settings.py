d845d9f3910b2859758b2a88508acf217e4ae0e4
André Cruz <andre@cabine.org>
2018-01-11 18:57:08 +0000

    Localization

--- webapp/webapp/settings.py
+++ webapp/webapp/settings.py
@@ -14,6 +14,8 @@ import os
 
 import structlog
 
+from django.utils.translation import gettext_lazy as _
+
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -148,6 +150,10 @@ AUTH_PASSWORD_VALIDATORS = [
 # https://docs.djangoproject.com/en/2.0/topics/i18n/
 
 LANGUAGE_CODE = 'en-us'
+LANGUAGES = [
+    ('pt-pt', _('Portuguese')),
+    ('en-us', _('English')),
+]
 
 TIME_ZONE = 'UTC'
 
